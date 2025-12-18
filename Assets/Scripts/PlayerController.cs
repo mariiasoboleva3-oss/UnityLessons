81c309c5f412e09e1f280db6157d490f5f47f256
UnityWOW <aleksandrkulyk@Air-Aleksandr.lan>
2025-12-18 18:27:08 +0100

    playersEtap

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+  private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rigidbody = GetComponent<Rigidbody>(); 
+       rigidbody.AddForce(Vector3.up * 6700);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//Je m'apelle Sacha 
+// Je suis en france
+// je veux apprendre l'allemand
