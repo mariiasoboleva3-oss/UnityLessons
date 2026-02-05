76aaae95b01ad42fd662684b34d3ebbeb218b761
UnityWOW <aleksandrkulyk@Air-Aleksandr.lan>
2026-02-05 18:36:02 +0100

    playerController logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
+
 {
+    public int speed;
+    public GameObject focalPoint;
+    private Rigidbody rigidBody; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+
     {
-        
+     float verticalInput = Input.GetAxis("Vertical");
+     rigidBody.AddForce(focalPoint.transform.forward * verticalInput * speed);
     }
+
 }
